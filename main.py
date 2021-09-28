95ad3c5dcfbebb3e5f98522595aa5a19e7b03612
chloedavis23 <818834@mystma.org>
2021-09-28 15:15:52 -0500

    5 - im just guessing at this point

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def close10(x, y):
     if x - y < abs(10): 
         return 10
 
-    elif y - x < abs(10): 
-        return 10
+    elif x == abs(10) == y: 
+        return 0
 
     else: 
         return 0
